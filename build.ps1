d716233adb9b29f8b1d4c1c93e1824c77a254983
David Carroll <carroll.david.s@gmail.com>
2025-05-13 07:49:17 -0500

    Increment version to 1.0.1, auto handle versioning tasks in build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
 param(
-    [Alias('v')][string]$version = ""
+    [Alias('v')][string]$version = "",
+    [Alias('c')][string]$changelog = ""
 )
 
+# Create the bin directory if it doesn't exist
 if (!(Test-Path -Path .\bin)) {
     New-Item -ItemType Directory -Path .\bin
 }
@@ -12,15 +14,38 @@ if ($version -ne "") {
     $filename = "smz3-ap-tracker-$version.zip"
 }
 
+# build the package
 if (Test-Path .\bin\$filename){
     Remove-Item .\bin\$filename -Force
 }  
 
 Compress-Archive -Path .\src -DestinationPath .\bin\$filename -Force
-
-$sha256 = Get-FileHash -Path .\bin\$filename -Algorithm SHA256
-
 Write-Host "Build complete. Output: .\bin\$filename"
-Write-Host "SHA256: $($sha256.Hash)"
+
+if ($version -ne "") {
+    $sha256 = Get-FileHash -Path .\bin\$filename -Algorithm SHA256
+    Write-Host "SHA256: $($sha256.Hash)"
+
+    # update the version in the manifest file
+    $manifestPath = ".\src\manifest.json"
+    $manifest = Get-Content -Path $manifestPath -Raw | ConvertFrom-Json
+    $manifest.package_version = $version
+    $manifest | ConvertTo-Json -Depth 10 | Set-Content -Path $manifestPath -Force
+
+    # add the new version to the versions.json file
+    $versionsPath = ".\versions.json"
+    $versions = Get-Content -Path $versionsPath -Raw | ConvertFrom-Json
+  
+    $newVersion = [PSCustomObject]@{
+        package_version = $version
+        download_url = "https://github.com/dessyreqt/smz3-ap-tracker/releases/download/$version/smz3-ap-tracker-$version.zip"
+        sha256  = $sha256.Hash
+        changelog = [array]@($changelog)
+    }
+    $versions.versions = @($newVersion) + $versions.versions
+    $versions | ConvertTo-Json -Depth 10 | Set-Content -Path $versionsPath -Force
+}
+
+
 
 
